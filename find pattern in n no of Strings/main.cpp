db952c963434873731f66b649ceb47e355e6082f
Navroop Singh Bajwa <navroop.bajwa@yahoo.com>
2019-01-28 20:24:23 +0530

    Initial Commit

---
+++ find pattern in n no of Strings/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  find pattern in n no of Strings
+//
+//  Created by Navroop Singh Bajwa on 28/01/19.
+//  Copyright © 2019 Navroop Singh Bajwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

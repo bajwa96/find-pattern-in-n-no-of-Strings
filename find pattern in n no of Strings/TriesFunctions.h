111ad71f53301d1a890afdb656b9d0321010ba75
Navroop Singh Bajwa <navroop.bajwa@yahoo.com>
2019-02-05 02:06:46 +0530

    Working code

--- find pattern in n no of Strings/TriesFunctions.h
+++ find pattern in n no of Strings/TriesFunctions.h
@@ -11,7 +11,11 @@
 
 #include<iostream>
 using namespace std;
-#define DEBUG_METHOD(name) std::cout << "-->" << name;
+
+#define ENTRY_LOG(name) std::cout << "Entrying -->" << name<<endl;
+#define EXIT_LOG(name) std::cout << "Exiting-->" << name<<endl;
+#define INFO(name) std::cout << "INFO-->" << name<<endl;
+
 struct ids{
     int current_id;
     struct ids *next,*prev;
@@ -21,65 +25,99 @@ struct ids{
         this->prev=NULL;
     }
 };
+struct trie{
+    //int idforsong; //to be used for song searching instead use head for ids as ids can be multiple
+    int count;
+    char data;
+    struct trie *next[26];
+    struct trie *number[10];
+    struct ids *head;
+    trie(char t){
+        for(int i=0;i<=26;i++)this->next[i]=NULL;
+        for(int i=0;i<=10;i++)this->number[i]=NULL;
+        this->data=t;
+        count=1;
+        this->head=NULL;
+    }
+};
 
 class make_search_easy{
     private :
-    struct trie{
-        //int idforsong; //to be used for song searching instead use head for ids as ids can be multiple
-        int count;
-        char data;
-        struct trie *next[26];
-        struct trie *number[10];
-        struct ids *head;
-        trie(char t){
-            for(int i=0;i<=26;i++)this->next[i]=NULL;
-            for(int i=0;i<=10;i++)this->number[i]=NULL;
-            this->data=t;
-            count=1;
-            this->head=NULL;
-        }
-    };
-    
     struct trie *headnumber[10]={NULL};//total=10 heads for numerics
     struct trie *head[26]={NULL};//total 26 heads for alphabets
-    public :
-    bool is_alpha(char t){
-        if(t>='a'&&t<='z')
+    
+    
+    /**
+     Checks if given alphabet is b/w 'a' and 'z'
+
+     @param alpha given alpha
+     @return value true/false
+     */
+    bool is_alpha(char alpha){
+        if(alpha>='a' && alpha<='z')
             return true;
         return false;
     }
+    
+    /**
+     Checks if given alphabet is b/w '0' and '9'
+     
+     @param alpha given number
+     @return value true/false
+     */
+    bool is_number(char alpha){
+        if(alpha>='0' && alpha<='9')
+            return true;
+        return false;
+    }
+    
     struct ids *add_id_for_song(int no,struct ids *head){
         if(head==NULL){
             return new ids (no);
         }else{
             struct ids *rethead=head;
-            int flag=0;
-            struct ids* last=head;
-            while(head){
-                last=head;
-                if(head->current_id==no){
-                    flag=1;
-                    break;
-                }
-                head=head->next;
+            // insertion in second pos in order to match with exsisting code
+            struct ids *posNextToFirst = head->next;
+            head->next =add_id_for_song(no,NULL);
+            if(head->next){
+                head->next->next=posNextToFirst;
             }
-            if(flag==0){
-                last->next=add_id_for_song(no, NULL);
-            }
+//
+//            int flag=0;
+//            struct ids* last=head;
+//            while(head){
+//                last=head;
+//                if(head->current_id==no){
+//                    flag=1;
+//                    break;
+//                }
+//                head=head->next;
+//            }
+//            if(flag==0){
+//                last->next=add_id_for_song(no, NULL);
+//            }
             return rethead;
         }
     }
     
+    /**
+     MAIN Fucntion that handles TRIES inserts given String into trie using recurssion
+
+     @param t given String
+     @param head head of t[0] of trie
+     @param song_id key associated with
+     @return returns tries head address for a-z or 0-9
+     */
     trie *perform_insertion(string t,trie *head,int song_id){ //perform from start to advance
         if(head==NULL){
-            struct trie *rethead=NULL,*agla=head;
+            struct trie *rethead=NULL,*agla=head; // agla -> here means will keep copy of previous pointer
             for(int i=0;i<t.length();i++){
                 if(rethead==NULL){
                     rethead=new trie (t[i]);
                     agla=rethead;
                     agla->head=add_id_for_song(song_id, agla->head);// addition for song id
                 }else{
-                    if(t[i]>='a'&&t[i]<='z'){
+                    if(is_alpha(t[i])){
                         agla->next[t[i]-'a']=new trie(t[i]);
                         agla=agla->next[t[i]-'a'];
                     }else{
@@ -89,28 +127,29 @@ class make_search_easy{
                     agla->head=add_id_for_song(song_id, agla->head);// addition for song id
                 }
             }
-            
             return rethead;
         }else{
             //match for same string till we get desired pos to insert and simply increase counter by one when we pass one
             trie *last=head;
             int index=0;
+            // iterating given trie to the point where insertion is required and adding ids to exsisting trie nodes
             while(index<t.length() && head!=NULL && head->data==t[index]){
                 last=head;
                 head->head=add_id_for_song(song_id, head->head);// addition for song id
                 last->count=last->count+1;
                 if(index+1<t.length()){
-                    if(t[index+1]>='a'&&t[index+1]<='z')
+                    if(is_alpha(t[index+1]))
                         head=head->next[t[index+1]-'a'];
                     else
                         head=head->number[t[index+1]-'0'];
                 }//else break;
                 index++;
             }
-            //head=last;
-            string remainingsub=t.substr(index,t.length());
+
+            string remainingsub=t.substr(index,t.length()); // remaining string not present in trie
+            
             if(remainingsub.length()>0){
-                if(remainingsub[0]>='a'&&remainingsub[0]<='z'){
+                if(is_alpha(remainingsub[0])){
                     if(head)
                         head->next[remainingsub[0]-'a']=perform_insertion(remainingsub, NULL,song_id);
                     else
@@ -129,37 +168,78 @@ class make_search_easy{
     }
     
     
-private: void insertt(string name,int key){
-    if(name.length()>0&&(name[0]>='a'&&name[0]<='z')){
-        if(head[name[0]-'a']==NULL){
-            head[name[0]-'a']=perform_insertion(name, head[name[0]-'a'],key);
-        }else{
-            perform_insertion(name, head[name[0]-'a'],key);
-        }
-    }else if(name.length()>0&&(name[0]>='1'&&name[0]<='9')){
-        if(headnumber[name[0]-'0']==NULL){
-            headnumber[name[0]-'0']=perform_insertion(name, headnumber[name[0]-'0'],key);
-        }else{
-            perform_insertion(name, headnumber[name[0]-'0'],key);
+    /**
+     inserts given string into trie and updates the respective head node for tries
+     rest insertion into trie is handleled by next function perform_insertion
+     @param name given string
+     @param key key associated with that string
+     */
+    void insertt(string name,int key){
+        
+        
+        if(is_alpha(name[0])){ // for english a-z
+            if(head[name[0]-'a']==NULL){
+                // initializes the head
+                head[name[0]-'a']=perform_insertion(name, head[name[0]-'a'],key);
+            }else{
+                // perform insertion is already present node
+                perform_insertion(name, head[name[0]-'a'],key);
+            }
+        }else { // for number 0-9
+            if(headnumber[name[0]-'0']==NULL){
+                // initializes the head
+                headnumber[name[0]-'0']=perform_insertion(name, headnumber[name[0]-'0'],key);
+            }else{
+                // perform insertion is already present node
+                perform_insertion(name, headnumber[name[0]-'0'],key);
+            }
         }
     }
-}
+
+    string getparsed(string t){
+        string ret="";
+        for(int i=0;i<t.length();i++){
+            if((t[i]>='a'&&t[i]<='z')||(t[i]>='0'&&t[i]<='9'))ret+=t[i];
+        }
+        return ret;
+    }
     
-    public :void insert(string t,int key){   //to be called for insertion purpose //MAIN INSERT Function
-        if(t.length()>0){
-            
-            string nonspaced="";
-            for(int i=0;i<t.length();i++){
-                if((t[i]>='a'&&t[i]<='z')||(t[i]>='1'&&t[i]<='9'))nonspaced=nonspaced+t[i];
-            }
-            for(int i=0;i<nonspaced.length();i++){
-                //cout<<i<<" "<<nonspaced.substr(i,nonspaced.length())<<endl;
-                if(nonspaced.substr(i,nonspaced.length()).length()>0)insertt(nonspaced.substr(i,nonspaced.length()), key);
-            }
+    void display_unique_ids(struct ids *head){
+        if(head){
+            while(head){
+                cout<<head->current_id<<" ";
+                head=head->next;
+            }cout<<endl;
+        }else cout<<"Empty";
+        
+    }
+    
+    void display_thishead(trie *head){
+        if(head!=NULL){
+            cout<<head->data<<" "<<head->count<<" | ";
+            display_unique_ids(head->head);
+            for(int i=0;i<26;i++)
+                if(head->next[i]!=NULL){
+                    display_thishead(head->next[i]);
+                    cout<<endl;
+                }
+            for(int i=0;i<10;i++)
+                if(head->number[i]!=NULL){
+                    display_thishead(head->number[i]);
+                    cout<<endl;
+                }
         }
     }
-    /*
-     insert new file"
+    
+    public :
+    
+    /**
+     Insert String associated with specific key for future identification
+
+     @param str string to be inserted
+     @param key associated key with string
+     
+     EX -> insert "new file"
      newfile
      ewfile
      wfile
@@ -169,15 +249,33 @@ private: void insertt(string name,int key){
      e
      
      */
-private: string getparsed(string t){
-    string ret="";
-    for(int i=0;i<t.length();i++){
-        if((t[i]>='a'&&t[i]<='z')||(t[i]>='0'&&t[i]<='9'))ret+=t[i];
+    void insert(string str,int key){
+        string name = "insert";
+        ENTRY_LOG(name);
+        
+        if(str.length()>0){
+            string nonspaced="";
+            for(int i=0;i<str.length();i++){
+                if(is_alpha(str[i])||is_number(str[i]))
+                    nonspaced=nonspaced+str[i];
+            }
+            for(int i=0;i<nonspaced.length();i++){
+                if(nonspaced.substr(i,nonspaced.length()).length()>0)
+                    insertt(nonspaced.substr(i,nonspaced.length()), key);
+            }
+        }
+        
+        EXIT_LOG(name);
     }
-    return ret;
-}
     
-    public :struct ids* search_for_this_string(string t){
+    struct ids* search_for_this_char(char ch){ // only made for is_alpha condition
+        if(is_alpha(ch)){
+            return head[ch-'a']?head[ch-'a']->head:NULL;
+        }else
+            return NULL;
+    }
+    
+    struct ids* search_for_this_string(string t){
         if(t.length()>0){
             
             t=getparsed(t);
@@ -185,20 +283,18 @@ private: string getparsed(string t){
             int index=0;
             // struct ids *rethead=NULL;
             char start=t[0];
-            if(start>='a'&&start<='z'&&head[start-'a']!=NULL){
+            if(is_alpha(start) && head[start-'a']!=NULL){
                 struct trie *temp=head[start-'a'],*last;
                 last=temp;
-                while(index<=t.length()&&temp!=NULL && temp->data==t[index] ){
+                while( index<=t.length() && temp!=NULL && temp->data==t[index] ){
                     last=temp;
-                    
                     if(t[index+1]>='a'&&t[index+1]<='z')
                         temp=temp->next[t[index+1]-'a'];
                     else if(t[index+1]>='0'&&t[index+1]<='1')
                         temp=temp->number[t[index+1]-'0'];
-                    
                     index++;
-                    //temp=temp->next[t[index]-'a'];
                 }
+                
                 if(t.substr(0,index).length()>0){
                     cout<<"Showing results for : \""<<t.substr(0,index)<<"\""<<endl;
                     if(temp)return temp->head;
@@ -232,35 +328,7 @@ private: string getparsed(string t){
         }
         return NULL;
     }
-    
-private:void display_unique_ids(struct ids *head){
-    if(head){
-        while(head){
-            cout<<head->current_id<<" ";
-            head=head->next;
-        }cout<<endl;
-    }else cout<<"Empty";
-    
-}
-    
-    
-    void display_thishead(trie *head){
-        if(head!=NULL){
-            cout<<head->data<<" "<<head->count<<" | ";
-            display_unique_ids(head->head);
-            for(int i=0;i<26;i++)
-                if(head->next[i]!=NULL){
-                    display_thishead(head->next[i]);
-                    cout<<endl;
-                }
-            for(int i=0;i<10;i++)
-                if(head->number[i]!=NULL){
-                    display_thishead(head->number[i]);
-                    cout<<endl;
-                }
-        }
-    }
-    public : void display(){
+    void display(){
         for (int i=0;i<26;i++)display_thishead(head[i]);
         for(int i=0;i<10;i++)display_thishead(headnumber[i]);
     }
@@ -274,6 +342,8 @@ private:void display_unique_ids(struct ids *head){
  for deleting in tries : mytrie.deletion_process( name to be deleted ,  uni_id associated with that string )
  for changing the id of song : mytrie.changing__id_process(name,oldid,newid to be assigned);
  */
+
+
 
 
 #endif /* TriesFunctions_h */
--- find pattern in n no of Strings/main.cpp
+++ find pattern in n no of Strings/main.cpp
@@ -8,14 +8,63 @@
 #include <iostream>
 #include <string>
 #include<vector>
+#include <map>
 #include "TriesFunctions.h"
 using namespace std;
+
+map<int,int> incrementOrInsertIntoMap(map<int,int> mapDaso, int id){
+    map<int,int>::iterator it;
+    it = mapDaso.find(id);
+    if(it != mapDaso.end()){
+        it->second = it->second+1;
+    }else{
+        mapDaso.insert(pair<int, int>(id,1));
+    }
+    return mapDaso;
+}
+
+map<int,int> *incrementOrInsertIntoMap(map<int,int> *mapDaso, int id){
+    map<int,int>::iterator it;
+    it = mapDaso->find(id);
+    if(it != mapDaso->end()){
+        it->second = it->second+1;
+    }else{
+        mapDaso->insert(pair<int, int>(id,1));
+    }
+    return mapDaso;
+}
+
+map<int, int> getCharCount(struct ids *head,map<int,int> *overAllMap){
+    
+    map<int, int> charCount;
+    
+    while(head){
+       charCount= incrementOrInsertIntoMap(charCount,head->current_id);
+        incrementOrInsertIntoMap(overAllMap,head->current_id);
+        head=head->next;
+    }
+    return charCount;
+}
+
+struct allDataForPair{
+    public :
+    map<int, int> charCount;
+    char alpha;
+    int count;
+    allDataForPair(struct ids *head,char alpha,map<int,int> *overAllMap){
+        charCount = getCharCount(head, overAllMap);
+        this->alpha=alpha;
+        this->count = 1;
+    }
+};
+
 int main() {
     
     make_search_easy mytrie;
     
     vector<string> words;
-    vector<ids*> wordsID;
+    vector<allDataForPair*> wordsID;
+    map<char,allDataForPair*> wordsIDmap;
     int noOfWords;
     
     cout<<"Enter no of words : ";
@@ -23,29 +72,38 @@ int main() {
     
     for(int i=0;i<noOfWords;i++){
         string word;
+        cout<<"Enter "<<i+1<<" word :";
         cin>>word;
         words.push_back(word);
-        DEBUG_METHOD("Now inserting into TRIES = " + word);
+        INFO("Now inserting into TRIES = " + word +"\n");
         mytrie.insert(word,i);
     }
 
     string pattern;
     cout<<"Enter pattern you want to search for : ";
     cin>>pattern;
-    
+    map<int,int> maxCount ;
     for(int i=0;i<pattern.length();i++){
-        DEBUG_METHOD("Now searching given String in Tries " + pattern.substr(0,i));
-        string charStr = pattern.at(i);
-        wordsID.push_back(mytrie.search_for_this_string(charStr));
+        wordsID.push_back(new allDataForPair(mytrie.search_for_this_char(pattern[i]),pattern[i],&maxCount));
     }
-    for(int i=0;i<wordsID.size();i++){
-        struct ids *head=wordsID.at(i);
-        while(head){
-            cout<<words.at(head->current_id)<<" ";
-            head=head->next;
+    
+    const int ansIndex = pattern.length()-1;
+    int longestLen=-1,index=-1;
+    map<int,int>::iterator it;
+    for (it=maxCount.begin(); it!=maxCount.end(); ++it){
+        cout << it->first << " => " << it->second << '\n';
+        if(it->second>=ansIndex){
+            cout<<"Found Word Fulfilling your given Cond :"<<words[it->first]<<endl;
+            if(words[it->first].length()>longestLen){
+                longestLen=words[it->first].length();
+                index=it->first;
+            }
         }
-        cout<<endl;
     }
+    cout<<"Final Ans = "<<words[it->first]<<endl;
+    
+    
+    
     
     return 0;
 }
